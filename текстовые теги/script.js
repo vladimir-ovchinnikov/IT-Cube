72a8eabf52f623c84466d4c596f9021e56d4e475
belnek <rubix7887@gmail.com>
2025-02-25 17:33:37 +0400

    изучение js

---
+++ текстовые теги/script.js
@@ -0,0 +1,149 @@
+var fullName ="Владимир";
+//int - целое число
+//flo
+//
+//str
+console.log(fullName)
+var old = "451"
+console.log(old)
+var a = 13;
+var b = 27;
+var S = a*b;
+console.log(S)
+var A = 24;
+var G = 12;
+var P = 24/12;
+console.log(P)
+var secundi = 300261;
+var normsecyndi = secundi % 60;
+var vseminuti = Math.round (secundi / 60);
+var minuti = vseminuti % 60;
+var vseoclock = Math.round (vseminuti / 60);
+var oclock = vseoclock % 60;
+var  vsetwentyfourhours = Math.round(vseoclock / 24);
+
+
+
+console.log(normsecyndi, minuti, oclock, vsetwentyfourhours )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
